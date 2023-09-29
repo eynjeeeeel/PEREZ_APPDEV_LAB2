bb5f47884d29aa9672111a2c8e43a6401c8d3369
angelicaaa <128461122+eynjeeeeel@users.noreply.github.com>
2023-09-29 07:13:52 -0700

    save files

--- app/Controllers/MainController.php
+++ app/Controllers/MainController.php
@@ -3,17 +3,19 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\MainModelPM;
 
 class MainController extends BaseController
 {
     private $playlists;
-    private $song;
+    private $music;
+    
 
 
     public function __construct()
     {
         $this->playlists = new \App\Models\MainModel1();
-        $this->song = new \App\Models\MainModel();
+        $this->music = new \App\Models\MainModel();
     }
     public function index()
     {
@@ -23,10 +25,10 @@ class MainController extends BaseController
     {
         $data = [
             'playlists' => $this->playlists->findAll(),
-            'song' => $this->song->findAll(),
+            'song' => $this->music->findAll(),
 
         ];
-        return view('view', $data);
+        return view ('view', $data);
     }
 
     public function createPlaylist()
@@ -39,31 +41,29 @@ class MainController extends BaseController
         return redirect()->to('/view');
     }
 
-    public function addsong()
+    public function addmusic()
     {
 
         $validationRules = [
-            'song' => 'uploaded[song]|max_size[song,10240]|mime_in[song,audio/mpeg,audio/wav]',
+            'music' => 'uploaded[music]|max_size[music,10240]|mime_in[music,audio/mpeg,audio/wav]',
         ];
         if ($this->validate($validationRules)) {
 
 
-            $song = $this->request->getFile('song');
-            $songname = $song->getName();
-            $newName = $song->getRandomName();
-            $song->move(ROOTPATH . 'uploads', $newName);
+            $music = $this->request->getFile('music');
+            $musicname = $music->getName();
+            $newName = $music->getRandomName();
+            $music->move(ROOTPATH . 'uploads', $newName);
             $data = [
-                'title' => $songname,
+                'title' => $musicname,
                 'file_path' => $newName,
                 //make sure tama names ng collumns
             ];
-            $this->song->insert($data);
+            $this->music->insert($data);
             return redirect()->to('/view');
         } else {
             $data['validation'] = $this->validator;
             echo "error";
         }
     }
-
-
 }
--- app/Views/view.php
+++ app/Views/view.php
@@ -118,18 +118,18 @@
 
 <body>
      <!-- //my addsong -->
-    <div class="modal fade" id="addsongs" tabindex="-1" aria-labelledby="addsongs" aria-hidden="true">
+     <div class="modal fade" id="addsongs" tabindex="-1" aria-labelledby="addsongs" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Add Song</h5>
+                    <h5 class="modal-title" id="exampleModalLabel">addSong</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
                     <br>
 
                     <form action="/addsong" method="post" enctype="multipart/form-data">
-                        <input type="file" name="music">
+                        <input type="file" name="song">
                         <button type="submit">Submit</button>
                     </form>
                 </div>
@@ -139,7 +139,6 @@
             </div>
         </div>
     </div>
-
 
     <!-- //add to playlist modal -->
     <div class="modal fade" id="exampleModalToggle" aria-hidden="true" aria-labelledby="exampleModalToggleLabel"
@@ -168,3 +167,152 @@
                     <div class="modal-footer">
                         <a href="#" data-bs-dismiss="modal">Close</a>
                         <a href="#" data-bs-target="#exampleModalToggle2" data-bs-toggle="modal">Create New</a>
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <div class="modal fade" id="exampleModalToggle2" aria-hidden="true" aria-labelledby="exampleModalToggleLabel2"
+        tabindex="-1">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalToggleLabel2">add to playlist</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form action="/createPlaylist" method="post">
+                        <input type="text" name="pname">
+                        <button type="submit">Submit</button>
+                    </form>
+                    </div>
+                <div class="modal-footer">
+                    <button class="btn btn-primary" data-bs-target="#exampleModalToggle" data-bs-toggle="modal"
+                        data-bs-dismiss="modal">Back to first</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- //add to playlist modal end -->
+
+
+
+    <!-- //main view :> -->
+    <form action="/" method="get">
+        <input type="search" name="search" placeholder="search song">
+        <button type="submit" class="btn btn-primary">search</button>
+    </form>
+    <h1>Music Player</h1>
+    <a class="btn btn-primary" data-bs-toggle="modal" href="#exampleModalToggle" role="button">Open Playlist</a>
+    <a class="btn btn-primary" data-bs-toggle="modal" href="#addsongs" role="button">Add Songs</a>
+    <song id="song" controls autoplay></song>
+    <ul id="playlist">
+    <?php foreach ($music as $music): ?>
+        <li data-src="<?= base_url('uploads/' . $music['file_path']) ?>">
+    <?= $music['title'] ?>
+</li>
+
+        <?php endforeach ?>
+
+    </ul>
+    <!-- //add to playlist -->
+    <div class="modal" id="myModal">
+        <div class="modal-dialog">
+            <div class="modal-content">
+
+                <!-- Modal Header -->
+                <div class="modal-header">
+                    <h4 class="modal-title">Select from playlist</h4>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                </div>
+
+                <!-- Modal body -->
+                <div class="modal-body">
+                    <form action="/" method="post">
+                        <!-- <p id="modalData"></p> -->
+                        <input type="hidden" id="musicID" name="music_id">
+                        <select name="playlists" class="form-control">
+
+                            <option value="playlists">playlist</option>
+
+                        </select>
+                        <input type="submit" name="add">
+                    </form>
+                </div>
+<!-- Modal footer -->
+<div class="modal-footer">
+                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <script>
+        $(document).ready(function () {
+            // Get references to the button and modal
+            const modal = $("#myModal");
+            const modalData = $("#modalData");
+            const musicID = $("#music_id");
+            // Function to open the modal with the specified data
+            function openModalWithData(dataId) {
+                // Set the data inside the modal content
+                modalData.text("Data ID: " + dataId);
+                musicID.val(dataId);
+                // Display the modal
+                modal.css("display", "block");
+            }
+
+            // Add click event listeners to all open modal buttons
+
+            // When the user clicks the close button or outside the modal, close it
+            modal.click(function (event) {
+                if (event.target === modal[0] || $(event.target).hasClass("close")) {
+                    modal.css("display", "none");
+                }
+            });
+        });
+    </script>
+    <script>
+const audio = document.getElementById('audio');
+        const playlists = document.getElementById('playlists');
+        const playlistItems = playlist.querySelectorAll('li');
+        let currentTrack = 0;
+
+function playTrack(trackIndex) {
+    if (trackIndex >= 0 && trackIndex < playlistItems.length) {
+        const track = playlistItems[trackIndex];
+        const trackSrc = track.getAttribute('data-src');
+        audio.src = trackSrc;
+        audio.play();
+        currentTrack = trackIndex;
+    }
+}
+
+function nextTrack() {
+    currentTrack = (currentTrack + 1) % playlistItems.length;
+    playTrack(currentTrack);
+}
+
+function previousTrack() {
+    currentTrack = (currentTrack - 1 + playlistItems.length) % playlistItems.length;
+    playTrack(currentTrack);
+}
+
+playlistItems.forEach((item, index) => {
+    item.addEventListener('click', () => {
+        playTrack(index);
+    });
+});
+ audio.addEventListener('ended', () => {
+            nextTrack();
+        });
+
+        playTrack(currentTrack);
+    </script>
+</body>
+
+</html>
+
